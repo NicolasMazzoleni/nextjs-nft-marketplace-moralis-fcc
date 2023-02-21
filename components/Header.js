cdd2b3e1234cdfec30effd96a8e07dba9357d44a
NICOLAS MAZZOLENI <mazzoleni.nicolas@gmail.com>
2023-02-21 10:20:27 +0100

    Add tailwind css styled navbar

--- components/Header.js
+++ components/Header.js
@@ -3,16 +3,19 @@ import Link from "next/link"
 
 export default function Header() {
     return (
-        <nav>
+        <nav className="p-5 border-b-2 flex flex-row justify-between items-center">
+            <h1 className="py-4 px-4 font-bold text-3xl">NFT Marketplace</h1> 
+            <div className="flex flex-row items-center">
             <Link legacyBehavior href="/">
-                <a>NFT Marketplace</a>
+                <a className="mr-4 p-6">Home</a>
                 </Link>
 
                 <Link legacyBehavior href="/sell-nft">
-                <a>Sell NFT</a>
+                <a className="mr-4 p-6">Sell NFT</a>
                 </Link>
-            <ConnectButton/>
+            <ConnectButton moralisAuth={false}/>
+            </div>
         </nav>
                 
     )
-}
+} 
